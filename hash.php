c4c7cba2ea10b840379b3d2965dcfec27dfcb90f
ArnikaRahman <arnikarahman03@gmail.com>
2026-01-28 22:42:43 +0600

    Add files via upload

---
+++ hash.php
@@ -0,0 +1,5 @@
+<?php
+echo "admin123 => " . password_hash("admin123", PASSWORD_DEFAULT) . "<br>";
+echo "teacher123 => " . password_hash("teacher123", PASSWORD_DEFAULT) . "<br>";
+echo "student123 => " . password_hash("student123", PASSWORD_DEFAULT) . "<br>";
+
